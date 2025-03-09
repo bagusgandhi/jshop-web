1d6f22bda163224d1d7393c4e0ac636e5a87d721
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-09 20:57:40 +0700

    feat: adding config nextjs for allowing image source

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Matches any domain
+      },
+    ],
+  },
+};
+
+export default nextConfig;
